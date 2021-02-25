5a5b72fcd8aa3598070ab88d4a6cfe0e77a693d1
hohoprince <hoot0512@gmail.com>
2021-02-26 02:18:25 +0900

    level1 신규아이디추천 추가, 테스트케이스 3개 남음

---
+++ programmers/level1/신규아이디추천.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+func solution(_ new_id:String) -> String {
+    var result = new_id.lowercased()
+    
+    do {
+        let regex = try NSRegularExpression(pattern: "[^a-z0-9-_.]")
+        result = regex.stringByReplacingMatches(in: result, range: NSRange(location: 0, length: result.count), withTemplate: "")
+    } catch {
+        print(error.localizedDescription)
+    }
+    
+    result = result.replacingOccurrences(of: "...", with: ".")
+    result = result.replacingOccurrences(of: "..", with: ".")
+    
+    result = result.trimmingCharacters(in: ["."])
+    
+    if result == "" {
+        result = "a"
+    }
+    
+    if result.count >= 16 {
+        result = String(result[result.startIndex...result.index(result.startIndex, offsetBy: 14)])
+    }
+    
+    result = result.trimmingCharacters(in: ["."])
+    
+    if result.count <= 2 {
+        let index = result.index(result.endIndex, offsetBy: -1)
+        while result.count < 3 {
+            result.append(result[index])
+        }
+    }
+    
+    return result
+}
+
+let new_id = "z-+.^."
+
+solution(new_id)
+
